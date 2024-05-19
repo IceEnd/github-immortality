b1256bce37f1a2e62598d3af9870194fd21f3962
IceEnd <min@coolecho.net>
2024-05-19 19:14:52 +0800

    feat: fog bg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
-export const MOUNTAIN = fs.readFileSync(path.resolve(__dirname, './mountain.svg'), 'utf-8');
 export const BRUSH = fs.readFileSync(path.resolve(__dirname, './brush.svg'), 'utf-8');
 export const PEOPLE = fs.readFileSync(path.resolve(__dirname, './people.svg'));
 export const PEOPLE_TEXTURE = fs.readFileSync(path.resolve(__dirname, './people-texture.svg'));
+export const FOG_SHIM = fs.readFileSync(path.resolve(__dirname, './fog-shim.svg'));
+export const FOG_SHIM_REVERSE = fs.readFileSync(path.resolve(__dirname, './fog-shim-reverse.svg'));
--- src/render/base.ts
+++ src/render/base.ts
@@ -1,4 +1,4 @@
-import { MOUNTAIN } from '../assets';
+import { FOG_SHIM, FOG_SHIM_REVERSE } from '../assets';
 import { IStats } from '../types/stats';
 
 export class Render {
@@ -31,10 +31,54 @@ export class Render {
 
   private renderBackground(): string {
     return `
+    <defs>
+      <filter id="fogFill" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feDropShadow dx="0" dy="8" stdDeviation="8" flood-color="#000" flood-opacity="0.3" />
+        <feGaussianBlur stdDeviation="8" />
+      </filter>
+      <filter id="fogShadow">
+        <feGaussianBlur result="gaussian_blur_7_12" stdDeviation="4"/>
+      </filter>
+      <linearGradient id="flogShadowFill" x1="0" y1="0" x2="0" y2="100%" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#000" stop-opacity="0.2" />
+        <stop offset="1" stop-color="#ccc" stop-opacity="0.2" />
+      </linearGradient>
+    </defs>
     <g stroke="none" fill="#f2f0eb">
       <rect width="${Render.WIDTH}" height="${Render.HEIGHT}" rx="${Render.CARD_RADIUS}" />
     </g>
-    ${MOUNTAIN}
+    <symbol id="Fog">${FOG_SHIM}</symbol>
+    <symbol id="FogReverse">${FOG_SHIM_REVERSE}</symbol>
+    <g opacity="0.6" tansform="translate(0, 0)">
+      <use href="#Fog" transform="scale(1.5) rotate(8)" x="40" y="10" />
+      <use href="#FogReverse" transform="scale(0.8, 1.5) rotate(-8)" x="-80" y="40" />
+      <use href="#Fog" transform="scale(1.1) rotate(8)" x="100" y="-20" />
+      <use href="#FogReverse" transform="scale(1.1, 1.2) rotate(-8)" x="-80" y="-20" />
+      <use href="#Fog" transform="scale(1.3, 0.5) rotate(10)" x="50" y="-50" />
+      <use href="#FogReverse" transform="scale(0.8)" x="-20" y="-40" />
+    </g>
+    <g>
+      <use href="#Fog" transform="rotate(8)" />
+      <animateTransform
+        attributeName="transform"
+        type="translate"
+        values="500 -20; -400 40;"
+        dur="30s"
+        repeatCount="indefinite"
+      />
+    </g>
+
+    <g>
+      <use href="#FogReverse" transform="rotate(-8)" />
+      <animateTransform
+        attributeName="transform"
+        type="translate"
+        values="-400 40; 500 20;"
+        dur="36s"
+        repeatCount="indefinite"
+      />
+    </g>
     `;
   }
+  // <!-- ${MOUNTAIN} -->
 }
